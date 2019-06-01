0666b3c7255172568062c0a22eebc347da19ddf6
John Williams <you@example.com>
2019-06-01 11:55:44 -0500

    Started implementing some stuff with bash scripts

--- app/js/flow.js
+++ app/js/flow.js
@@ -1,5 +1,26 @@
 const path = require('path');
 var ffmpeg = require('ffmpeg-static');
-$(document).ready(function () {
-	$(".main").append(ffmpeg.path);
-});
+
+function appendTxt(className, txt){
+	$(document).ready(function () {
+		$("." + className).append(txt);
+	});
+}
+
+function runCmd(command) {
+	var exec = require('child_process').exec, child;
+	child = exec(command,
+	function (error, stdout, stderr) {
+		console.log('stdout: ' + stdout);
+		appendTxt("main", '<br><p class="test">Stdout:<br>' + stdout + '</p>');	
+		console.log('stderr: ' + stderr);
+		appendTxt("main", '<br><p class="test">Stderr:<br>' + stderr + '</p>');	
+		if (error !== null) {
+		console.log('exec error: ' + error);
+		}
+	});
+}
+
+appendTxt("main", "ffmpeg static path: " + ffmpeg.path);
+runCmd(ffmpeg.path + 
+	' -i "/media/internal4tb/Television/ameridad/Family Guy - season\'s 1-9/Family Guy - Season 2/202 - Holy Crap.avi"');
