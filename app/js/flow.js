72e1bf8e09c1ac82e507ba54ff3da805ee3ae74f
John Williams <you@example.com>
2019-06-01 12:39:22 -0500

    Little functioning stream display. Now going to test the packaging

--- app/js/flow.js
+++ app/js/flow.js
@@ -21,6 +21,51 @@ function runCmd(command) {
 	});
 }
 
+//Clears the html within an element
+function clearHtml(elemId) {
+        $("." + elemId).html("");
+}
+
+filePaths = [];
+//Processes input files from button
+function fileUp(){
+        filePaths = [];
+        //clear screen when new files are presented
+        clearHtml("main");
+        //clearHtml("ffOut");
+        var x = document.getElementById("upFile");
+        var txt = "";
+        if ('files' in x) {
+        if (x.files.length == 0) {
+                txt = "Select one or more files.";
+        } else {
+                for (var i = 0; i < x.files.length; i++) {
+                        txt += "<br><strong>" + (i+1) + ".</strong><br>";
+                        var file = x.files[i];
+                        if ('name' in file) {
+				filePaths.push(file.path);
+                                txt += "Path: " + file.path + "<br>";
+                        }
+                        if ('size' in file) {
+                                txt += "size: " + file.size + " bytes <br>";
+                        }
+                }
+        }
+        }
+        else {
+                if (x.value == "") {
+                        txt += "Select one or more files.";
+                } else {
+                        txt += "The files property is not supported by your browser!";
+                        // If the browser does not support the files property, it will return the path of the selected file instead.
+                        txt  += "<br>The path of the selected file: " + x.value;                }
+        }
+	appendTxt("clipForm", txt);
+	for (i = 0; i < filePaths.length; i++){
+		tempPath = filePaths[i];
+                runCmd('./app/bash/streaminfo.sh ' + '"' + tempPath + '"');
+        }
+}
+
 appendTxt("main", "ffmpeg static path: " + ffmpeg.path);
-runCmd(ffmpeg.path + 
-	' -i "/media/internal4tb/Television/ameridad/Family Guy - season\'s 1-9/Family Guy - Season 2/202 - Holy Crap.avi"');
+
