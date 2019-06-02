e7b22f4dea5fbf0987feb4dd0a2c9a9f33e45ac6
John Williams <you@example.com>
2019-06-01 20:59:50 -0500

    Working on building the forms

--- app/js/flow.js
+++ app/js/flow.js
@@ -4,6 +4,8 @@ var ffmpeg = require('ffmpeg-static');
 var ffpath = ffmpeg.path;
 var filePaths = [];
 var films = [];
+var filmCount = 0;
+var clipCount = 0;
 
 class Stream {
 	constructor(streams, meta){
@@ -13,8 +15,9 @@ class Stream {
 }
 
 class Film {
-        constructor(filepath, video, audio, subtitle, extSubs) {
-                this.filepath = filepath;
+        constructor(id, filepath, video, audio, subtitle, extSubs) {
+                this.id = id;
+		this.filepath = filepath;
                 this.video = video;
                 this.audio = audio;
                 this.subtitle = subtitle;
@@ -22,19 +25,115 @@ class Film {
         }
 }
 
-function appendTxt(className, txt){
+function appendTxt(name, txt){
 	$(document).ready(function () {
-		$("." + className).append(txt);
+		$(name).append(txt);
+	});
+}
+function radioCheck(name, val){
+	$(document).ready(function () {
+		$(name).prop("checked", true);
 	});
 }
 
 //Clears the html within an element
 function clearHtml(elemId) {
-        $("." + elemId).html("");
+        $(elemId).html("");
+}
+
+function filmForm(film){
+	var id = film.id;
+	appendTxt(".clipForm", '<br><div id="inputDiv-' + id +'" class="inputDiv"></div>');
+	appendTxt("#inputDiv-" + id, '<form method="post" id="form-' + id + '"></form>');
+	appendTxt("#form-" + id, "<b>Filename: " + path.basename(film.filepath) + "</b><br><br>"); 
+
+	//Video Choice
+	if (film.video.length == 0){
+		appendTxt("#form-" + id, "<b>No Video Streams</b><br>");
+	} else{
+		appendTxt("#form-" + id, "<b>Video Streams</b><br>");
+		for (var i = 0; i < film.video.length; i++){
+			appendTxt("#form-" + id, '<input type="radio" name="vStreams-' + id + '" ' + 
+			'id="vStreams-' + i + '-' + id + '" value="' + i + '">' + film.video[i] + '<br>');
+			if(i == 0){
+				radioCheck("#vStreams-" + i + "-" + id, true);
+			}
+		}
+	}
+	appendTxt("#form-" + id, "<br>");
+
+	//Audio Choice
+	if (film.audio.length == 0){
+		appendTxt("#form-" + id, "<b>No Audio Streams</b><br>");
+		appendTxt("#form-" + id, '<input type="radio" name="aStreams-' + id + '" ' + 
+		'id="aStreams-noaudio-"' + id + '" value="-1">No Audio<br>');
+		radioCheck("#aStreams-noaudio-" + id, true);
+	} else{
+		appendTxt("#form-" + id, "<b>Audio Streams</b><br>");
+		appendTxt("#form-" + id, '<input type="radio" name="aStreams-' + id + '" ' + 
+		'id="aStreams-noaudio-' + id + '" value="-1"> No Audio<br>');
+		for (var i = 0; i < film.audio.length; i++){
+			appendTxt("#form-" + id, '<input type="radio" name="aStreams-' + id + '" ' + 
+			'id="aStreams-' + i + '-' + id + '" value="' + i + '">' + film.audio[i] + '<br>');
+			if(i == 0){
+				radioCheck("#aStreams-" + i + "-" + id, true);
+			}
+		}
+	}
+	appendTxt("#form-" + id, "<br>");
+
+	//Subtitle Choice
+	if (film.subtitle.length + film.extSubs.length == 0){
+		appendTxt("#form-" + id, "<b>No Subtitles Available</b><br>");
+		appendTxt("#form-" + id, '<input type="radio" name="sStreams-' + id + '" ' + 
+		'id="aStreams-nosub-"' + id + '" value="-1">No Subtitles<br>');
+		radioCheck("#aStreams-nosub-" + id, true);
+	} else{
+		appendTxt("#form-" + id, "<b>Subtitle Streams</b><br>");
+		appendTxt("#form-" + id, '<input type="radio" name="sStreams-' + id + '" ' + 
+		'id="sStreams-' + i + '-' + id + '" value="-1">No Subtitles<br>');
+		radioCheck("#sStreams-" + i + "-" + id, true);
+		for (var i = 0; i < film.subtitle.length + film.extSubs.length; i++){
+			if (i < film.subtitle.length){
+				appendTxt("#form-" + id, '<input type="radio" name="aStreams-' + id + '" ' + 
+				'id="aStreams-' + i + '-' + id + '" value="' + i + '">' + film.subtitle[i] + '<br>');
+			} else{
+				var extInd = i - film.subtitle.length;
+				appendTxt("#form-" + id, '<input type="radio" name="sStreams-' + id + '" ' + 
+				'id="sStreams-' + i + '-' + id + '" value="' + i + '"> ' + 
+				path.basename(film.extSubs[extInd]) + '<br>');
+			}
+		}
+	}
+	appendTxt("#form-" + id, "<br>");
+
+	//start time
+	appendTxt("#form-" + id, '<div class="formRow" id="start-' + id + '"></div>');
+	appendTxt("#start-" + id, '<div class="formColumn" id="startCol-' + id + '"></div>');
+	
+	//duration
+	appendTxt("#form-" + id, '<div class="formRow" id="dur-' + id + '">></div>');
+	
+
+	//crf value
+	appendTxt("#form-" + id, '<div class="formRow" id="crf-' + id + '">></div>');
+	
+
+	//scale
+	
+
+	//crop
+	
+
+	//clip name
+	
+
+	//submit button
+
 }
 
 function streamProcess(results, filepath) {
-	//appendTxt("main", results);
+	//appendTxt(".main", results);
 	var streams = [];
 	var vStreams = [];
 	var aStreams = [];
@@ -46,13 +145,13 @@ function streamProcess(results, filepath) {
 	var sReg = /.*Subtitle:.*/;
 	var lines = results.split("\n");
 	for (var i = 0; i < lines.length; i++){
-		//appendTxt("main", lines[i] + "<br>");
+		//appendTxt(".main", lines[i] + "<br>");
 		if (streamReg.test(lines[i])){
 			streams.push(lines[i]);
 		}
 	}
 	for (var i = 0; i < streams.length; i++){
-		//appendTxt("main", streams[i] + "<br>");
+		//appendTxt(".main", streams[i] + "<br>");
 		if (vReg.test(streams[i])){
 			vStreams.push(streams[i]);
 		} else if (aReg.test(streams[i])){
@@ -74,8 +173,10 @@ function streamProcess(results, filepath) {
 	console.log("Audio Streams: " + aStreams);
 	console.log("Subtitle Streams: " + sStreams);
 	console.log("External Subtitles: " + extSubs);
-	var newFilm = new Film(filepath, vStreams, aStreams, sStreams, extSubs)
+	var newFilm = new Film(filmCount, filepath, vStreams, aStreams, sStreams, extSubs)
 	films.push(newFilm);
+	filmCount++;
+	filmForm(newFilm);
 }
 
 function ffprobe(filepath) {
@@ -92,11 +193,13 @@ function ffprobe(filepath) {
 	});
 }
 
+
 //Processes input files from button
 function fileUp() {
+	films = [];
         filePaths = [];
         //clear screen when new files are presented
-        clearHtml("main");
+        clearHtml(".clipForm");
         //clearHtml("ffOut");
         var x = document.getElementById("upFile");
         var txt = "";
@@ -125,13 +228,13 @@ function fileUp() {
                         // If the browser does not support the files property, it will return the path of the selected file instead.
                         txt  += "<br>The path of the selected file: " + x.value;                }
         }
-	appendTxt("clipForm", txt);
+	//appendTxt(".clipForm", txt);
 	for (var i = 0; i < filePaths.length; i++){
 		var tempPath = filePaths[i];
 		ffprobe(tempPath);
         }
 }
 
-appendTxt("main", "ffmpeg static path: " + ffmpeg.path);
+//appendTxt(".main", "ffmpeg static path: " + ffmpeg.path);
 
 
